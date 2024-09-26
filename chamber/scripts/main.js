7c91ea8b0ccd4160695c2c633d55a4380fdff062
alexcy1 <cyr23001@byui.edu>
2024-09-26 09:40:35 +0100

    Cahmber of Commerce

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -102,8 +102,8 @@ document.addEventListener('DOMContentLoaded', function () {
             heroTextOverlay.style.marginLeft = '12.1px'; 
 
         } else if (window.innerWidth < 992) {
-            heroImage.style.marginLeft = '11.5px';
-            heroTextOverlay.style.marginLeft = '11.5px';
+            heroImage.style.marginLeft = '12.1px';
+            heroTextOverlay.style.marginLeft = '12.1px';
 
         } else {
             heroImage.style.marginLeft = '0'; // Reset margin for larger screens
